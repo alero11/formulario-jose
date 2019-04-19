969e9a179b8c76db5310225c8af23f95d7875d35
Alex Rolando Toro Benitez <alex_rolando@hotmail.com>
2019-04-19 09:21:53 -0400

    adicon de los componentes bootstrap, jquery  y material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+// configuracion de animaciones
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// material
+import { MaterialModule } from './core/material/material.module';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
